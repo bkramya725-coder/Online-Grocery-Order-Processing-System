de9703c1dbf92281f08073237b95215c251ff993
bkramya725-coder <bkramya725@gmail.com>
2025-12-27 22:37:10 +0530

    Update PurchaseOrderItemServiceImpl.java

--- online_grocery/springapp/src/main/java/com/examly/springapp/service/PurchaseOrderItemServiceImpl.java
+++ online_grocery/springapp/src/main/java/com/examly/springapp/service/PurchaseOrderItemServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.examly.springapp.exception.BadRequestException;
+import com.examly.springapp.exception.ResourceNotFoundException;
 import com.examly.springapp.model.Product;
 import com.examly.springapp.model.PurchaseOrderItem;
 import com.examly.springapp.repository.ProductRepo;
@@ -17,14 +19,31 @@ public class PurchaseOrderItemServiceImpl implements PurchaseOrderItemService{
 
     @Autowired
     ProductRepo productRepo;
-    @Override
-    public PurchaseOrderItem addPurchaseOrderItem(PurchaseOrderItem purchaseOrderItem) {
-        Long pid=purchaseOrderItem.getProduct().getProductId();
-        Product p=productRepo.findById(pid).orElse(null);
-        purchaseOrderItem.setProduct(p);
-        return purchaseOrderItemRepo.save(purchaseOrderItem);
-        
+   @Override
+public PurchaseOrderItem addPurchaseOrderItem(PurchaseOrderItem purchaseOrderItem) {
+
+    
+    if  (purchaseOrderItem == null ||
+    purchaseOrderItem.getProduct() == null ||
+    purchaseOrderItem.getProduct().getProductId() == null) {
+        throw new BadRequestException("Product ID must be provided");
     }
+
+    Long pid = purchaseOrderItem.getProduct().getProductId();
+
+   
+    Product product = productRepo.findById(pid)
+        .orElseThrow(() ->
+            new ResourceNotFoundException(
+                "Product not found with id: " + pid
+            )
+        );
+
+    purchaseOrderItem.setProduct(product);
+
+    return purchaseOrderItemRepo.save(purchaseOrderItem);
+}
+
 
     public List<PurchaseOrderItem> getByOrderId(Long orderId){
     return purchaseOrderItemRepo
@@ -35,3 +54,4 @@ public class PurchaseOrderItemServiceImpl implements PurchaseOrderItemService{
   
     
 }
+
